db50e909cd21dc5b53590ca453d22c017b6bf7e9
Chao Steven Zhang <steven.zhang@goboxer.com>
2022-09-08 00:18:06 +1200

    build layout

---
+++ src/component/logIn.tsx
@@ -0,0 +1,42 @@
+import { Button, Input, Typography } from "antd";
+import React, { useState } from "react";
+
+const { Title } = Typography;
+
+interface LogInProps {}
+
+function LogIn({}: LogInProps) {
+  const [userName, setUserName] = useState<string | undefined>();
+  const [logInDisabled, setLogInDisabled] = useState<boolean>(true);
+
+  const goToMainPage = () => {
+    console.log("jump to register credit card page");
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(e.target.value);
+    setLogInDisabled(!Boolean(e.target.value));
+  };
+
+  return (
+    <div className="login-container">
+      <Title className="asb-logo">ASB</Title>
+      <Input
+        status={logInDisabled ? "warning" : ""}
+        value={userName}
+        onChange={handleNameChange}
+        placeholder="Name"
+      />
+      <Button
+        className="margin-top-20px"
+        disabled={logInDisabled}
+        type="primary"
+        onClick={goToMainPage}
+      >
+        Log in
+      </Button>
+    </div>
+  );
+}
+
+export default LogIn;
---
+++ src/component/main.tsx
@@ -0,0 +1,152 @@
+import {
+  Button,
+  DatePicker,
+  Drawer,
+  Form,
+  Input,
+  Layout,
+  Menu,
+  Typography,
+} from "antd";
+import { BarsOutlined, LogoutOutlined } from "@ant-design/icons";
+import type { DatePickerProps, MenuProps } from "antd";
+import React, { useState, useEffect } from "react";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+const { Title } = Typography;
+const { Header, Content } = Layout;
+interface MainProps {}
+
+const monthFormat = "MM/YYYY";
+
+function Main({}: MainProps) {
+  const [cardNumber, setCardNumber] = useState<number | undefined>();
+  const [expireDate, setExpireDate] = useState<string>("");
+  const [CVC, setCVC] = useState<number | undefined>();
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const [submitDisabled, setSubmitDisabled] = useState<boolean>(true);
+
+  useEffect(() => {
+    const disabled = !Boolean(cardNumber && expireDate && CVC);
+    setSubmitDisabled(disabled);
+  }, [cardNumber, expireDate, CVC]);
+
+  const onExpireDateChange: DatePickerProps["onChange"] = (_, dateString) => {
+    setExpireDate(dateString);
+  };
+
+  const getNumber = (value: any) => {
+    if (isNaN(parseInt(value))) {
+      return undefined;
+    }
+
+    return parseInt(value);
+  };
+
+  const onCardNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setCardNumber(getNumber(value));
+  };
+
+  const onCVCChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setCVC(getNumber(value));
+  };
+
+  const onFinish = () => {
+    console.log({ cardNumber, expireDate, CVC });
+  };
+
+  const onMenuClick: MenuProps["onClick"] = (e) => {
+    console.log("click", e);
+    // jump to log in page.
+  };
+
+  const items: MenuItem[] = [
+    {
+      key: "log_out",
+      icon: <LogoutOutlined />,
+      label: "Log out",
+    },
+  ];
+
+  return (
+    <div className="main-container">
+      <Drawer
+        title="Menu"
+        placement="left"
+        onClose={() => setMenuOpen(false)}
+        open={menuOpen}
+      >
+        <Menu
+          onClick={onMenuClick}
+          style={{ width: 256 }}
+          mode="vertical"
+          items={items}
+        />
+      </Drawer>
+      <Header className="main-header">
+        <BarsOutlined
+          className="margin-right-auto"
+          onClick={() => setMenuOpen(true)}
+        />
+        <div className="margin-right-auto">Register card form</div>
+      </Header>
+      <Content className="content-container">
+        <Title level={5} className="main-content-title">
+          Welcome {"Steven"}
+        </Title>
+        <Form
+          className="main-credit-card-form"
+          labelCol={{ span: 10 }}
+          wrapperCol={{ span: 14 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Card Number"
+            rules={[{ required: true, message: "Please input your username" }]}
+          >
+            <Input
+              value={cardNumber}
+              onChange={onCardNumberChange}
+              placeholder="Card Number"
+              style={{ width: "100%" }}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Expiration Date"
+            rules={[
+              { required: true, message: "Please select an expiration date" },
+            ]}
+          >
+            <DatePicker
+              format={monthFormat}
+              picker="month"
+              onChange={onExpireDateChange}
+              style={{ width: "100%" }}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="CVC"
+            rules={[{ required: true, message: "Please input CVC" }]}
+          >
+            <Input value={CVC} onChange={onCVCChange} placeholder="CVC" />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button disabled={submitDisabled} type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Content>
+    </div>
+  );
+}
+
+export default Main;
